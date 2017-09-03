3d3280524b5b0724ec27392de5b16044f994771b
Jeremy Waffles <jer.5683@gmail.com>
2017-09-03 23:30:31 +0000

    Fixed the appearance of home page

--- Config.php
+++ Config.php
@@ -2,22 +2,13 @@
     /**
     //Connect to the database
     $host = "127.0.0.1";
-    $user = "jerwaffles";                     //Your Cloud 9 username
-    $pass = "";                                  //Remember, there is NO password by default!
-    $db = "VilleMarie";                                  //Your database name you want to connect to
-    $port = 3306;                                //The port #. It is always 3306
+    $user = "ryaechuvpz";      
+    $pass = "3RV3QH2KBv";      
+    $db = "ryaechuvpz";        
+    $port = 3306;              
     
     $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
 
-
-
-    //And now to perform a simple query to make sure it's working
-    $query = "SELECT * FROM Users";
-    $result = mysqli_query($connection, $query);
-
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "The ID is: " . $row['id'] . " and the Username is: " . $row['username'];
-    }
 */
 
     define('DB_SERVER', '127.0.0.1');
